198aaeb6b0bd2e19d1cd2738bd3c6e7fabd44922
Athullya-PD <athullyapd@gmail.com>
2025-07-29 14:19:49 +0530

    Update TestKhan.java

--- src/test/java/testpackage/TestKhan.java
+++ src/test/java/testpackage/TestKhan.java
@@ -1,5 +1,7 @@
 package testpackage;
 
+import java.io.IOException;
+
 import org.testng.annotations.Test;
 import basepackage.BaseKhan;
 import pagepackage.PageKhan;
@@ -7,7 +9,7 @@ import pagepackage.PageKhan;
 public class TestKhan extends BaseKhan {
 
     @Test
-    public void loginClickTest() {
+    public void loginClickTest() throws IOException {
         PageKhan ob = new PageKhan(driver);
         ob.loginm(); 
         ob.scrollDown();
@@ -16,6 +18,14 @@ public class TestKhan extends BaseKhan {
         ob.dropdown();
         ob.signupclick();
         ob.signupdetails();
-        //ob.signinginn();
+        ob.signinginn();
+        ob.backbuttonClick();
+        ob.scrollDown();
+        ob.loginWithSignupCredentials();
+        ob.selectCustomRangeWithKeyboard();
+        ob.datepicker();
+       ob.printPageTitle();
+       ob.closeCurrentTab();
     }
 }
+
